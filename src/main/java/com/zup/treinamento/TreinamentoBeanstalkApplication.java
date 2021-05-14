4a9db254200a4e68304eaba1f177d92ebc8ea5c3
carloslopeszup <carlos.lopes@zup.com.br>
2021-05-14 00:09:05 -0300

    teste

---
+++ src/main/java/com/zup/treinamento/TreinamentoBeanstalkApplication.java
@@ -0,0 +1,13 @@
+package com.zup.treinamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinamentoBeanstalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinamentoBeanstalkApplication.class, args);
+	}
+
+}
